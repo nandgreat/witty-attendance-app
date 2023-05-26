cccad490d44b280423f6510ea912e38d51c1fb7c
Nandgreat <paul4nank@gmail.com>
2023-05-26 10:21:17 +0100

    Still troubleshooting cors error

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\Cors::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
