57b750ab09df7256b76980c540a320b31aded254
Nandgreat <paul4nank@gmail.com>
2023-06-03 12:01:20 +0100

    Allow origin updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://coza-keylog-dashboard-6ve1.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
